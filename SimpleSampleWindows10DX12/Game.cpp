55b66435d8ef7e6292536cbc2f70fd9875069314
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-06-15 11:58:59 -0700

    Updated DirectXTK12 to June 15, 2020 release

--- SimpleSampleWindows10DX12/Game.cpp
+++ SimpleSampleWindows10DX12/Game.cpp
@@ -343,8 +343,6 @@ void Game::CreateDeviceDependentResources()
     m_states = std::make_unique<CommonStates>(device);
 
     m_resourceDescriptors = std::make_unique<DescriptorHeap>(device,
-        D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-        D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
         Descriptors::Count);
 
     m_batch = std::make_unique<PrimitiveBatch<VertexPositionColor>>(device);
