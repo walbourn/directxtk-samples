32352d4591aff91a6e3d76f65ec244802fe7e094
Chuck Walbourn <chuckw@microsoft.com>
2021-02-26 23:11:30 -0800

    Update copyright banner

--- SimpleSampleWindows10_XAML/Content/DirectXTK3DSceneRenderer.cpp
+++ SimpleSampleWindows10_XAML/Content/DirectXTK3DSceneRenderer.cpp
@@ -10,7 +10,8 @@
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
 // PARTICULAR PURPOSE.
 //
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License (MIT).
 //--------------------------------------------------------------------------------------
 
 #include "pch.h"
--- SimpleSampleWindows10_XAML/Content/DirectXTK3DSceneRenderer.h
+++ SimpleSampleWindows10_XAML/Content/DirectXTK3DSceneRenderer.h
@@ -5,12 +5,8 @@
 //
 // http://go.microsoft.com/fwlink/?LinkId=248929
 //
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License (MIT).
 //--------------------------------------------------------------------------------------
 
 #pragma once
