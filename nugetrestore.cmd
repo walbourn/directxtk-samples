e008f674232b1bb3dc5266a97549bdd17d984290
Chuck Walbourn <chuckw@microsoft.com>
2022-11-19 17:59:38 -0800

    D3DX12 updated for Agility SDK 1.608

---
+++ nugetrestore.cmd
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+set NUGET=c:\util\nuget.exe
+if NOT exist %NUGET% goto error
+echo .
+for /D %%1 in (*) do (pushd %%1
+%NUGET% restore "DirectXTKSimpleSample.sln"
+popd)
+popd
+goto :eof
+:error
+echo ERROR cannot find %NUGET%
