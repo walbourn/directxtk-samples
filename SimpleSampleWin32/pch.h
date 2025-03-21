677cefeee84d813589f187ba773c5b3c3d21f733
Chuck Walbourn <chuckw@microsoft.com>
2025-03-21 16:54:16 -0700

    Update for October 2025 release

--- SimpleSampleWin32/pch.h
+++ SimpleSampleWin32/pch.h
@@ -6,7 +6,9 @@
 #pragma once
 
 #include <winsdkver.h>
-#define _WIN32_WINNT 0x0601
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0603
+#endif
 #include <sdkddkver.h>
 
 // Use the C++ standard templated min/max
